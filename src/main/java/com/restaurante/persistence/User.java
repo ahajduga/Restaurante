be408cca77c51aed09a43c635f733a7b06d8203c
adamh <adam.hajduga@hotmail.com>
2016-04-05 16:51:51 +0200

    properties file v2 hibernate + postgresql

--- src/main/java/com/restaurante/persistence/User.java
+++ src/main/java/com/restaurante/persistence/User.java
@@ -1,12 +1,13 @@
 package com.restaurante.persistence;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 
 /**
  * Created by alex on 29.03.16.
  */
 @Entity
+@javax.persistence.Table(name = "sys_users")
 public class User {
 @Id
     private String login;
