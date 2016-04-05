0cf4bc4a1c118fe825374805229309a61945cef7
adamh <adam.hajduga@hotmail.com>
2016-04-05 16:00:21 +0200

    properties file v1

--- src/main/java/com/restaurante/persistence/User.java
+++ src/main/java/com/restaurante/persistence/User.java
@@ -1,10 +1,14 @@
 package com.restaurante.persistence;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * Created by alex on 29.03.16.
  */
+@Entity
 public class User {
-
+@Id
     private String login;
     private int type;
 }
