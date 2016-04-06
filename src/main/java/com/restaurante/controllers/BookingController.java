43a1a2bc1dbe6188af4e8b160ee4f0af1cb588c8
adamh <adam.hajduga@hotmail.com>
2016-04-06 11:35:03 +0200

    rest2

--- src/main/java/com/restaurante/controllers/BookingController.java
+++ src/main/java/com/restaurante/controllers/BookingController.java
@@ -6,7 +6,7 @@ import com.restaurante.persistence.dao.BookingDao;
 import com.restaurante.persistence.dao.TableDao;
 import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -16,6 +16,7 @@ import java.util.*;
 /**
  * Created by alex on 29.03.16.
  */
+@RestController
 public class BookingController {
 
     @Autowired
@@ -23,6 +24,35 @@ public class BookingController {
 
     @Autowired
     BookingDao bookingDao;
+@RequestMapping(value = "/book/{id}", method= RequestMethod.GET)
+    public Map<String,String> bookTable(
+            @PathVariable Integer tableID,
+            @RequestParam(value="from", required = true) String dateFrom,
+            @RequestParam(value="to", required = true) String dateTo) throws ParseException {
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd-HH", Locale.ENGLISH);
+        Date from = format.parse(dateFrom);
+        Date to = format.parse(dateTo);
+Map<String,String> resultMap = new HashMap<>();
+        for(Booking b : tableDao.findOne((long)tableID).getBookings()){
+        if(!b.isActive()){
+            if((!b.getDateStart().before(from) && !b.getDateStart().after(to)) || !b.getDateEnd().after(to) && !b.getDateEnd().before(from)){
+                resultMap.put("msg", "invalid");
+            }
+            else{
+                Table table = tableDao.findOne((long)tableID);
+                Booking bookingToSave = new Booking();
+                bookingToSave.setActive(true);
+                bookingToSave.setDateStart(from);
+                bookingToSave.setDateEnd(to);
+                table.getBookings().add(bookingToSave);
+                tableDao.save(table);
+                resultMap.put("msg","ok");
+            }
+        }
+        }
+        return null;
+    }
+
 
     public TreeMap<Date, Date> getFreeBookings(
             @RequestParam(value = "table_ID", required = true) Integer tableID,
