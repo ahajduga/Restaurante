5a2f9a77da85cef9c0d4847d9e9684a76fcd8ebc
alex <a.kocieniewski@gmail.com>
2016-04-28 21:17:34 +0200

    security update

--- src/main/java/com/restaurante/security/SecurityConfiguration.java
+++ src/main/java/com/restaurante/security/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 /**
  * Created by alex on 04.05.15.
@@ -39,6 +40,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 //                .antMatchers(HttpMethod.PUT, "/**").hasRole("EMPLOYEE")
                 .antMatchers(HttpMethod.PATCH, "/**").hasRole("ANONYMOUS").and()
                 .csrf().disable();
+
+        http.headers().addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
     }
 
     @Override
