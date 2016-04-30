15e8b8a9694500e6bff7244bdc8a013478ecd0ac
trobinpl <195947@student.pwr.edu.pl>
2016-04-30 16:56:37 +0200

    Very bad and very big commit with base reservation form and some dashboard code

---
+++ src/main/resources/static/app/controllers.js
@@ -0,0 +1,54 @@
+var restauranteControllers = angular.module('restauranteControllers', []);
+
+restauranteApp.controller('TableController', ['$scope', 'FreeTable', 'Date', 'Seatmap', '$http', '$resource', function($scope, FreeTable, Date, Seatmap, $http, $resource){
+    Seatmap.registerClickHandler();
+
+    $scope.hourChange = function(){
+        $("#noTablesError").hide();
+        Seatmap.deselectTables();
+        var tableFrom = Date.getRestDate($scope.hour);
+        var tableTo = Date.getRestDate(parseInt($scope.hour)+1);
+        $scope.tables = FreeTable.query({from: tableFrom, to: tableTo});
+        var loader = $('#loader');
+        var room = $('#room');
+        room.fadeOut();
+        $('#selectHourInfo').hide();
+        loader.show();
+        var tablesSvg = document.getElementById("seats").contentDocument;
+
+        angular.forEach($scope.tables, function(value, key)
+            {
+                var tableId = "table-"+value.tableID;
+                if(value.seats == 0){
+                    var table = $(tablesSvg.getElementById(tableId));
+                    table.addClass('table-taken');
+                    table.attr('available', false);
+                }
+            }
+        );
+
+        setTimeout(function(){loader.fadeOut(200); room.fadeIn(200)}, 1000);
+    }
+
+    $scope.reserve = function(){
+        var IDsToReserve = Seatmap.getSelectedTablesIDs();
+        if(IDsToReserve.length == 0) {
+            $("#noTablesError").show();
+            return;
+        }
+        $('#waitingModal').modal('show');
+    }
+}]);
+
+restauranteControllers.controller('DashboardController', ['$scope', 'Book', 'Date', function($scope, Book, Date){
+    var bookings = Book.getLatestBookings().query({from: Date.getRestDate("00")});
+    $scope.dashboard = {
+        bookings: bookings,
+        totalBookings: 101
+    };
+}]);
+
+restauranteControllers.controller('StaffController', ['$scope', 'Staff', function($resource, Staff){
+    var users = Staff.getStaff().query();
+    $scope.staff = users;
+}]);
